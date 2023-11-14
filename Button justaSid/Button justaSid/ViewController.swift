385081790d3b6de3543bb3f6ed2d23848fc3680e
balandzin <balandzin2602@gmail.com>
2023-11-14 20:46:20 +0300

    Создал игру ПРАВДА ИЛИ ДЕЙСТВИЕ

--- Button justaSid/Button justaSid/ViewController.swift
+++ Button justaSid/Button justaSid/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var truthButton: UIButton!
+    @IBOutlet var actionButton: UIButton!
+    
+    @IBOutlet var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
+    
+    
+    @IBAction func truthAction(_ sender: UIButton) {
+        textLabel.text = Source.getInfo(type: .word)
+    }
+    
+    @IBAction func actionAction(_ sender: Any) {
+        textLabel.text = Source.getInfo(type: .action)
+    }
+    
 }
 
